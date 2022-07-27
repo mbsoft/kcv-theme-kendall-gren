16214e4a8ec6ed9bb931903e434e82f982a2da45
Jim Welch <jim.welch@nextbillion.ai>
2022-07-27 11:13:42 -0400

    Updates

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function render(resumeObject) {
                         e.endDateYear += " (expected)";
                     }
                 } else {
-                    e.endDateYear = 'Present'
+                    e.endDateYear = '';
                     e.endDateMonth = '';
                 }
                 if (e.courses) {
